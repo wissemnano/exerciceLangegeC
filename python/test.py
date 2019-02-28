2efc06c8fee16b5b2815910601dcb93180f56a25
wissem <wissem.mansouri.ing@gmail.com>
2019-02-28 19:59:03 +0100

    correction final de la commande cmd

--- python/test.py
+++ python/test.py
@@ -6,8 +6,7 @@ import os # On importe le module os qui dispose de variables
 
 # Programme testant si une année, saisie par l'utilisateur, est bissextile ou non
 
-os.popen("cd Desktop\Git")
-os.popen("mkdir tcdsg")
+os.popen("cd C:/Users/MANSOURI/Desktop/Git & mkdir tcdsg")
 
 # On met le programme en pause pour éviter qu'il ne se referme (Windows)
 os.system("pause")
