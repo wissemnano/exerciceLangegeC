f8afcece2bdb1ce4c1e71e85edc64e06ca5270e3
wissem <wissem.mansouri.ing@gmail.com>
2019-02-28 19:32:26 +0100

    ajouter cmd commande in  python file

---
+++ python/test.py
@@ -0,0 +1,12 @@
+# -*-coding:utf-8 -*
+
+import os # On importe le module os qui dispose de variables 
+          # et de fonctions utiles pour dialoguer avec votre 
+          # système d'exploitation
+
+# Programme testant si une année, saisie par l'utilisateur, est bissextile ou non
+
+os.popen("type nul > desktop/file.txt")
+
+# On met le programme en pause pour éviter qu'il ne se referme (Windows)
+os.system("pause")
