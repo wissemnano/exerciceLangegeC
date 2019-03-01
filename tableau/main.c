ed3d607cb26c9c89ee1c6f2f9b9510fe87fc7c44
wissem <wissem.mansouri.ing@gmail.com>
2019-03-01 15:20:57 +0100

    creation fonction sommeTableau

--- tableau/main.c
+++ tableau/main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int sommeTableau(int *tableau, int tailleTableau);
+
 int main()
 {
-    printf("Hello world!\n");
+
     return 0;
+}
+
+int sommeTableau(int *tableau, int tailleTableau)
+{
+    int somme=0;
+    for(int i=0; i<tailleTableau; i++)
+    {
+        somme=somme+*(tableau+i);
+    }
+    return somme;
 }
